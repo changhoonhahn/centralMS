72018886bae825947ae6a296afa3b9a0ee30029a
changhoonhahn <chh327@nyu.edu>
2016-07-06 17:01:23 +0000

    integrated SFR stellar masses being implemented

--- code/centralms.py
+++ code/centralms.py
@@ -57,9 +57,26 @@ class CentralMS(object):
                 setattr(self, col, grp[col][:])
 
         for key in grp.attrs.keys(): 
-            setattr(self, key+'_attr', grp.attrs[key])
+            if key in ['sfms', 'tau']: 
+                attr_dict = {}
+                for keyind in grp.attrs[key].split(','): 
+                    try: 
+                        attr_dict[keyind.split(':')[0]] = float(keyind.split(':')[1])
+                    except ValueError:
+                        attr_dict[keyind.split(':')[0]] = keyind.split(':')[1]
+
+                if key == 'tau': 
+                    attr_dict['name'] = 'line'
+
+                setattr(self, key+'_dict', attr_dict)
+            else:
+                setattr(self, key+'_attr', grp.attrs[key])
 
         f.close() 
+            
+        # some small pre-processing here so convenience 
+        setattr(self, 'zsnap_genesis', UT.z_from_t(self.tsnap_genesis)) 
+
         return None 
 
 
@@ -70,24 +87,91 @@ def AssignSFR0(cms):
     if 'tsnap_genesis' not in cms.__dict__.keys(): 
         # Most likely you did not read in CenQue catalog!
         raise ValueError
-    
-    z_genesis = UT.z_from_t(cms.tsnap_genesis) 
 
     # Assign SFR to star-forming galaxies 
-    sfms_dict = {}
-    for keyind in cms.sfms_attr.split(','): 
-        try: 
-            sfms_dict[keyind.split(':')[0]] = float(keyind.split(':')[1])
-        except ValueError:
-            sfms_dict[keyind.split(':')[0]] = keyind.split(':')[1]
-
-    mu_logsfr = sfrs.AverageLogSFR_sfms(cms.mass_genesis, z_genesis, sfms_dict=sfms_dict)
-    sigma_logsfr = sfrs.ScatterLogSFR_sfms(cms.mass_genesis, z_genesis, sfms_dict=sfms_dict)
-
-    sfr_genesis = mu_logsfr + sigma_logsfr
-    cms.sfr_genesis = sfr_genesis
+    mu_logsfr = sfrs.AverageLogSFR_sfms(cms.mass_genesis, cms.zsnap_genesis, 
+            sfms_dict=cms.sfms_dict)
+    sigma_logsfr = sfrs.ScatterLogSFR_sfms(cms.mass_genesis, cms.zsnap_genesis, 
+            sfms_dict=cms.sfms_dict)
+    cms.sfr_genesis = mu_logsfr + sigma_logsfr
 
     return cms 
+
+
+
+class Evolver(object): 
+    
+    def __init__(self, cms, evol_dict=None): 
+        ''' Class object that evolves the CentralMS galaxy catalog catalog object .
+        Object contains suite of functions for the evolution. 
+        '''
+        self.cms = cms 
+        if evol_dict is None:  # default 
+            self.evol_dict = {
+                    'sfr': {'name': 'nothing'}, 
+                    'mass': {'type': 'rk4', 'f_retain': 0.6, 't_step': 0.1} 
+                    }
+        else: 
+            self.evol_dict = evol_dict
+        self.Evolve()
+
+    def __call__(self):  
+        return self.cms 
+
+    def Evolve(self, evol_dict=None): 
+        ''' Evolve SFR and calculated integrated SFR stellar masses. 
+        The module creates lambda functions for log SFR(t) and then 
+        integrates that. Currently set up to minimize the amount of
+        specific cases. 
+        '''
+        # Construct functions of SFR(t) 
+        sfr_dict = self.evol_dict['sfr']
+        if sfr_dict['name'] == 'nothing':
+            # No assembly bias. No dutycycle
+            mu_logsfr = sfrs.AverageLogSFR_sfms(
+                    self.cms.mass_genesis, UT.z_from_t(self.cms.tsnap_genesis), 
+                    sfms_dict=self.cms.sfms_dict)
+            dsfr = self.cms.sfr_genesis - mu_logsfr
+
+            self.logSFRt_MS = lambda mstar, t: sfrs.SFRt_MS_nothing(mstar, t, dsfr, 
+                    sfms_dict=self.cms.sfms_dict) 
+        else: 
+            raise NotImplementedError
+    
+        # Now integrate to get stellar mass 
+        mass_dict = self.evol_dict['mass']
+
+        qing = np.where(self.cms.t_quench != 999.)  # quenching galaxies 
+    
+        t_final = np.repeat(13.1328, len(self.cms.mass_genesis))
+        t_final[qing] = self.cms.t_quench[qing]
+    
+        # integrated SFR stellar mass while on SFMS 
+        # for galaxies that do not quench, this is their final mass  
+        integ_mass, sfr_f = sfrs.integSFR(self.logSFRt_MS, self.cms.mass_genesis, 
+                self.cms.tsnap_genesis, t_final, mass_dict=mass_dict) 
+    
+        self.cms.sfr = sfr_f 
+        self.cms.mass = integ_mass
+        t_final = np.repeat(13.1328, len(self.cms.mass_genesis))
+
+        # quenching galaxies after they're off the SFMS
+        if sfr_dict['name'] == 'nothing':
+            self.logSFRt_Q = lambda mstar, t: sfrs.SFRt_Q_nothing(
+                    self.cms.mass[qing], t, dsfr[qing], 
+                    sfms_dict=self.cms.sfms_dict, tau_dict=self.cms.tau_dict)
+        else: 
+            raise NotImplementedError
+    
+        # calculate finall stellar mass of quenching galaxies
+        integ_mass_Q, sfrQ_f= sfrs.integSFR(self.logSFRt_Q, self.cms.mass[qing], 
+                self.cms.t_quench[qing], t_final[qing], mass_dict=mass_dict) 
+        self.cms.mass[qing] = integ_mass_Q
+        self.cms.sfr[qing] = sfrQ_f 
+
+        return None
+
+
 
 
 if __name__=='__main__': 
@@ -95,4 +179,5 @@ if __name__=='__main__':
     cms._Read_CenQue()
 
     blah = AssignSFR0(cms)
-    print blah.sfr_genesis
+    eev = Evolver(blah)
+    new_blah = eev()
--- code/sfrs.py
+++ code/sfrs.py
@@ -4,8 +4,11 @@ Functions for handling star formation rates
 
 
 '''
+import time
 import numpy as np 
 
+# --- local --- 
+import util as UT
 
 
 def AverageLogSFR_sfms(mstar, z_in, sfms_dict=None): 
@@ -60,3 +63,170 @@ def ScatterLogSFR_sfms(mstar, z_in, sfms_dict=None):
     if sfms_dict is None: 
         raise ValueError
     return 0.3 
+
+
+def integSFR(logsfr, mass0, t0, tf, mass_dict=None):
+    ''' Integrated star formation rate stellar mass using Euler or RK4 integration
+
+    M* = M*0 + f_retain * Int(SFR(t) dt, t0, tf)
+    
+    Parameters
+    ----------
+    logsfr : function 
+        SFR function that accepts mass and t_cosmic as inputs 
+
+    mass : ndarray
+        initial stellar mass  
+
+    t0 : ndarray 
+        initial cosmic time 
+
+    tf : ndarray
+        final cosmic time
+
+    f_retain : 
+        fraction of stellar mass not lost from SNe and winds from Wetzel Paper
+    '''
+    type = mass_dict['type']            # Euler or RK4
+    f_retain = mass_dict['f_retain']    # Retain fraction 
+    delt = mass_dict['t_step']          # maximum t resolution of the integral 
+
+    niter = int(np.round( (tf.max()-t0.min())/delt ))+1
+    
+    t_n_1 = t0 
+    logSFR_n_1 = logsfr(mass0, t0)
+    logM_n_1 = mass0
+    
+    if niter > 0: 
+        print niter, ' ', type, ' iterations'
+        print 'f_reatin = ', f_retain
+
+        for i in xrange(niter): 
+            iter_time = time.time()
+            t_n = t_n_1 + delt
+            print tf.max() 
+            keep = np.where(tf > t_n) 
+
+            if len(keep[0]) == 0: 
+                continue
+                
+            if type == 'rk4': 
+                k1 = (10.0 ** logSFR_n_1)
+    
+                print len(logM_n_1), len(t_n_1)
+                k2_sfr = logsfr(
+                        np.log10(10.0**logM_n_1 + (10**9 * delt)/2.0 * k1), 
+                        t_n_1 + delt/2.0)
+                k2 = (10.0 ** k2_sfr)
+
+                k3_sfr = logsfr(
+                        np.log10(10.0**logM_n_1 + (10**9 * delt)/2.0 * k2), 
+                        t_n_1 + delt/2.0)
+                k3 = (10.0 ** k3_sfr)
+
+                k4_sfr = logsfr(
+                        np.log10(10.0**logM_n_1 + (10**9 * delt) * k3), 
+                        t_n_1 + delt)
+                k4 = (10.0 ** k4_sfr)
+
+                logM_n_1[keep] = np.log10(10.0 ** logM_n_1[keep] + f_retain/6.0 * (delt * 10**9) * (k1[keep] + 2.0*k2[keep] + 2.0*k3[keep] + k4[keep])) 
+
+            elif type == 'euler': 
+                logM_n_1[keep] = np.log10((10. ** logM_n_1[keep]) + delt * 10.**9. * f_retain * (10.** logSFR_n_1[keep]))
+            else: 
+                raise NotImplementedError
+            
+            if np.sum(np.isnan(logM_n_1)) > 0: 
+                raise ValueError('There are NaNs') 
+    
+            # update log(SFR), and t from step n-1
+            logSFR_n_1[keep] = logsfr(logM_n_1, t_n)[keep]
+            t_n_1 = t_n
+
+    # sanity check
+    if np.min(logM_n_1 - mass0) < 0.0: 
+        if np.min(logM_n_1 - mass0) > -0.001: 
+            pass
+        else: 
+            raise ValueError("integrated mass cannot decrease over cosmic time")
+
+    return logM_n_1, logSFR_n_1
+
+
+def SFRt_MS_nothing(mstar, t, dsfr, sfms_dict=None):
+    ''' log SFR(t) for the `nothing` scenario
+    '''
+    print t.max()
+    mu_logsfr = AverageLogSFR_sfms(mstar, UT.z_from_t(t), sfms_dict=sfms_dict)
+    return mu_logsfr + dsfr
+
+
+def SFRt_Q_nothing(MQ, t, dsfr, tQ=None, sfms_dict=None, tau_dict=None): 
+    ''' log SFR(t) after tQ to tf for quenching galaxies (NOTE THAT THIS IS VERY SPECIFIC)
+                                
+    log(SFR)_quenching = np.log10( np.exp( -(t_f - t_Q)/tau) )
+    '''
+    mu_logsfr = AverageLogSFR_sfms(MQ, UT.z_from_t(tQ), sfms_dict=sfms_dict)
+
+    tauQ = getTauQ(MQ, tau_dict=tau_dict)
+    
+    dlogsfrq = np.log10( np.exp( (tq - t) / tauQ ) ) 
+    
+    return mu_logsfr + dsfr + dlogsfrq  
+
+def getTauQ(mstar, tau_dict=None): 
+    ''' Return quenching efold based on stellar mass of galaxy, Tau(M*).
+    '''
+    type = tau_dict['name']
+    if type == 'constant':      # constant tau 
+
+        n_arr = len(mstar) 
+        tau = np.array([0.5 for i in xrange(n_arr)]) 
+
+    elif type == 'linear':      # lienar tau(mass) 
+
+        tau = -(0.8 / 1.67) * ( mstar - 9.5) + 1.0
+        #if np.min(tau) < 0.1: #    tau[ tau < 0.1 ] = 0.1
+         
+    elif type == 'instant':     # instant quenching 
+
+        n_arr = len(mstar) 
+        tau = np.array([0.001 for i in range(n_arr)]) 
+
+    elif type == 'discrete': 
+        # param will give 4 discrete tau at the center of mass bins 
+        masses = np.array([9.75, 10.25, 10.75, 11.25]) 
+
+        if param is None: 
+            raise ValueError('asdfasdfa') 
+
+        tau = np.interp(mstar, masses, param) 
+        tau[ tau < 0.05 ] = 0.05
+
+    elif type == 'line': 
+        # param will give slope and yint of pivoted tau line 
+        
+        tau = tau_prop['slope'] * (mstar - tau_prop['fid_mass']) + tau_prop['yint']
+        
+        try: 
+            if np.min(tau) < 0.001: 
+                tau[np.where( tau < 0.001 )] = 0.001
+        except ValueError: 
+            pass 
+
+    elif type == 'satellite':   # quenching e-fold of satellite
+
+        tau = -0.57 * ( mstar - 9.78) + 0.8
+        if np.min(tau) < 0.001:     
+            tau[np.where( tau < 0.001 )] = 0.001
+
+    elif type == 'long':      # long quenching (for qa purposes)
+
+        n_arr = len(mstar) 
+        tau = np.array([2.0 for i in xrange(n_arr)]) 
+
+    else: 
+        raise NotImplementedError('asdf')
+
+    return tau 
+
