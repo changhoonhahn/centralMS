bb88b8c959d6d4991b16462e31e73cd09528fe1f
chang.nersc <changh20@gmail.com>
2017-08-29 16:51:05 -0700

    some issues with the tdelay_dt_mcmc

--- centralms/tests/test_tdelay_dt.py
+++ centralms/tests/test_tdelay_dt.py
@@ -25,19 +25,6 @@ def tdelay_dt_mcmc(run, theta, Niter=20, Nwalkers=10, Ndim=2, sigma_smhm=0.2, ns
         tdelay_range = [0., 3.]#np.arange(0., 3., 0.5)
         dt_range = [0.1, 4.]
 
-    smhmr = Obvs.Smhmr()
-    def sigM(tt):
-        theta_i = np.concatenate([theta, np.array([tt[0], tt[1]])])
-        subcat_sim = abcee.model(run, theta_i, 
-                nsnap0=nsnap0, sigma_smhm=sigma_smhm, downsampled=downsampled) 
-        sumsim = abcee.SumSim(['smf'], subcat_sim, info=True)
-        
-        isSF = np.where(subcat_sim['gclass'] == 'sf') # only SF galaxies 
-        # calculate sigma_M* at M_h = 12
-        m_mid, mu_mhalo, sig_mhalo, cnts = smhmr.Calculate(subcat_sim['halo.m'][isSF], subcat_sim['m.star'][isSF], 
-                dmhalo=0.2, weights=subcat_sim['weights'][isSF])
-        return sig_mhalo[np.argmin(np.abs(m_mid-12.))]
-
     # new chain 
     chain_file = ''.join([UT.fig_dir(), run, '.tdelay_dt_mcmc.chain.dat']) 
     if os.path.isfile(chain_file) and continue_chain:   
@@ -46,7 +33,7 @@ def tdelay_dt_mcmc(run, theta, Niter=20, Nwalkers=10, Ndim=2, sigma_smhm=0.2, ns
         Niter = Niter - (np.float(len(sample))/np.float(Nwalkers)) # Number of chains left to finish 
         if Niter <= 0: 
             raise ValueError
-            print Nchain, ' iterations left to finish'
+            print Niter, ' iterations left to finish'
     else: 
         f = open(chain_file, 'w')
         f.close()
@@ -59,8 +46,14 @@ def tdelay_dt_mcmc(run, theta, Niter=20, Nwalkers=10, Ndim=2, sigma_smhm=0.2, ns
         sys.exit(0)
 
     # Initializing the emcee sampler
-    sampler = emcee.EnsembleSampler(Nwalkers, Ndim, sigM, pool=pool)
-    for result in sampler.sample(pos0, iterations=Nchain, storechain=False):
+    kwargs = {
+            'theta': theta, 
+            'sigma_smhm': 0.2, 
+            'nsnap0': 15, 
+            'downsampled': '14', 
+            }
+    sampler = emcee.EnsembleSampler(Nwalkers, Ndim, sigM, pool=pool, kwargs=kwargs)
+    for result in sampler.sample(pos0, iterations=Niter, storechain=False):
         position = result[0]
         #print position
         f = open(chain_file, 'a')
@@ -71,6 +64,20 @@ def tdelay_dt_mcmc(run, theta, Niter=20, Nwalkers=10, Ndim=2, sigma_smhm=0.2, ns
     pool.close()
 
     return None 
+
+    
+def sigM(tt, theta=np.array([1.35, 0.6]), sigma_smhm=0.2, nsnap0=15, downsampled='14'):
+    smhmr = Obvs.Smhmr()
+    theta_i = np.concatenate([theta, np.array([tt[0], tt[1]])])
+    subcat_sim = abcee.model(run, theta_i, 
+            nsnap0=nsnap0, sigma_smhm=sigma_smhm, downsampled=downsampled) 
+    sumsim = abcee.SumSim(['smf'], subcat_sim, info=True)
+
+    isSF = np.where(subcat_sim['gclass'] == 'sf') # only SF galaxies 
+    #calculate sigma_M* at M_h = 12
+    m_mid, mu_mhalo, sig_mhalo, cnts = smhmr.Calculate(subcat_sim['halo.m'][isSF], subcat_sim['m.star'][isSF], 
+            dmhalo=0.2, weights=subcat_sim['weights'][isSF])
+    return sig_mhalo[np.argmin(np.abs(m_mid-12.))]
 
 
 if __name__=='__main__': 
