a260f95c66025f3640e8f13ca346c30ec12b45b8
changhoonhahn <chh327@nyu.edu>
2017-10-11 00:43:49 +0000

    prepping harmattan for another run

--- run/harmattan_constantoffset.sh
+++ run/harmattan_constantoffset.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 # run constant offset with updated evolver and SFMS treatment.
-mpirun -np 10 python ../centralms/run_abcee.py test0 10 1000 1 1 
+mpirun -np 10 python ../centralms/run_abcee.py test0 3 100 1 0 
