815ff13cc8f5ec875b531bc1bf053ac115a34a28
ChangHoon Hahn <chh327@nyu.edu>
2017-10-11 22:48:01 -0700

    moved some files still haven't figured out why the new model is so discrepant

--- centralms/tests/test_abcee.py
+++ centralms/tests/test_abcee.py
@@ -493,7 +493,7 @@ def test_tduty_tdelay_dt_grid_dtaxis(run, tdutyy, tdelayy):
 if __name__=='__main__': 
     #test_SFMS_highz('test0', 9, nsnap=15, lit='lee')
 
-    test_SumSim('test0')#'rSFH_r1.0_most')
+    #test_SumSim('test0')#'rSFH_r1.0_most')
     #test_SumSim_sigmaSMHM('rSFH_r1.0_most', sigma_smhm=0.0)
     #for rr in [0.1, 0.33, 0.66, 0.99]: 
     #    test_dMh_dMstar('rSFH_r_delay_dt_test', np.array([1.35, 0.6, rr, 0.5, 0., 0.5]), sigma_smhm=0.2, flag='R'+str(rr))
