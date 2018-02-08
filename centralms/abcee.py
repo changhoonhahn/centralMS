0dba291d161b89bae75118610afe63e1ff64b95b
Chang Hoon Hahn <hahn@sirocco.cosmo.fas.nyu.edu>
2018-02-08 11:15:08 -0500

    running correlated SFH tdyn

--- centralms/abcee.py
+++ centralms/abcee.py
@@ -246,6 +246,17 @@ def model(run, args, **kwargs):
         theta['sfh']['sigma_corr'] = 0.99 * 0.3
         theta['sfh']['dt_delay'] = 0. # Gyr 
         theta['sfh']['dt_dMh'] = 2.5 # Gyr
+    elif run == 'rSFH_r0.99_tdyn_0.5gyr_narrSFMS': 
+        # random SFH with 0.99 correlation with halo growth 
+        # over t_dyn and duty cycle of 0.5 Gyr 
+        theta['sfh'] = {'name': 'random_step_abias_delay_dt'}
+        theta['sfh']['dt_min'] = 0.5 
+        theta['sfh']['dt_max'] = 0.5 
+        theta['sfh']['sigma_tot'] = 0.2 # note narrower SFMS
+        theta['sfh']['sigma_corr'] = 0.99 * 0.2 # note narrower SFMS
+        theta['sfh']['dt_delay'] = 0. # Gyr 
+        theta['sfh']['dt_dMh'] = 2.5 # Gyr
+        theta['sfms']['sigma'] = 0.2 # note narrower SFMS
     elif run == 'rSFH_r0.99_delay_dt_test': 
         theta['sfh'] = {'name': 'random_step_abias_delay_dt'}
         theta['sfh']['dt_min'] = args[2]
