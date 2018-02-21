6f36565e7a6678d50f3aac45b92864927acf8d23
Chang Hoon Hahn <hahn@sirocco.cosmo.fas.nyu.edu>
2018-02-21 12:43:19 -0500

    running realizations of ABC posterior

--- centralms/tests/test_abcee.py
+++ centralms/tests/test_abcee.py
@@ -552,9 +552,10 @@ if __name__=='__main__':
     #sfh_name = 'rSFH_r0.99_delay'
     #sfh_name = 'test0' # 'randomSFH_short'
 
-    sfh_name = 'rSFH_r0.99_tdyn_0.5gyr_narrSFMS'#'randomSFH_5gyr' # 'randomSFH_short'
-    #sfh_name = 'randomSFH_5gyr_narrSFMS' # 'randomSFH_short'
+    #sfh_name = 'rSFH_r0.99_tdyn_0.5gyr_narrSFMS'#'randomSFH_5gyr' # 'randomSFH_short'
+    sfh_name = 'randomSFH_2gyr'#_narrSFMS' # 'randomSFH_short'
+    #sfh_name = 'rSFH_r0.5_tdyn_0.5gyr'
     #test_model_ABCparticle(sfh_name, 13)
-    for t in [13]: #range(1,12)[::-1]: #[5,6]: #range(5):
+    for t in [14]: #range(1,12)[::-1]: #[5,6]: #range(5):
         test_plotABC(sfh_name, t)
         test_qaplotABC(sfh_name, t)
