ac41a15e5eaff6eb5e55c7e9eb14088515e04be4
Chang Hoon Hahn <hahn@sirocco.cosmo.fas.nyu.edu>
2019-03-29 19:50:31 -0400

    running 0.45 dex z1 sigma on sirocco

--- run/siro_sigmaz1.sh
+++ run/siro_sigmaz1.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 #PBS -l nodes=1:ppn=24
-#PBS -N abc_r0.0_t0.5_sigz1
+#PBS -N abc_r0.99_t0.5_sigz1
 #PBS -m bea
 #PBS -M changhoonhahn@lbl.gov
 cd $PBS_O_WORKDIR
@@ -13,11 +13,11 @@ export CENTRALMS_CODEDIR="/home/users/hahn/projects/centralMS/"
 # this script is to generate the abc posteriors for testing what
 # happens when we change sigma_logM* at z=1. 
 
-tduty="0.5"
+tduty="10"
 sfs="broken"
 
 # no assembly bias (i.e. r = 0) 
-mpirun -np $NPROCS python /home/users/hahn/projects/centralMS/run/abc.py noabias_z1sigma $tduty $sfs 0.35 15 1000 > "/home/users/hahn/projects/centralMS/run/rSFH_noabias_"$tduty"gyr.sigma_z1_0.35.sfs"$sfs".log"
+#mpirun -np $NPROCS python /home/users/hahn/projects/centralMS/run/abc.py noabias_z1sigma $tduty $sfs 0.45 15 1000 > "/home/users/hahn/projects/centralMS/run/rSFH_noabias_"$tduty"gyr.sigma_z1_0.45.sfs"$sfs".log"
  
 # yes assembly bias (i.e. r > 0) 
-#mpirun -np $NPROCS python /home/users/hahn/projects/centralMS/run/abc.py abias_z1sigma 0.99 $tduty $sfs 0.35 15 1000 > "/home/users/hahn/projects/centralMS/run/rSFH_abias0.99_"$tduty"gyr.sigma_z1_0.35.sfs"$sfs".log"
+mpirun -np $NPROCS python /home/users/hahn/projects/centralMS/run/abc.py abias_z1sigma 0.99 $tduty $sfs 0.45 15 1000 > "/home/users/hahn/projects/centralMS/run/rSFH_abias0.99_"$tduty"gyr.sigma_z1_0.45.sfs"$sfs".log"
